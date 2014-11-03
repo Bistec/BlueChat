1a32ae7725616f27575a44692ebb2186e86bbeb8
Edward J Skrod <ejs09f@my.fsu.edu>
2014-11-03 16:31:49 -0500

    init commit

---
+++ src/com/example/bluetoothchat/BlueChat_Welcome.java
@@ -0,0 +1,59 @@
+package com.example.bluetoothchat;
+
+import com.example.android.BluetoothChat.R;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class BlueChat_Welcome extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue_chat__welcome);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Prisma.otf");
+        TextView BlueChat = (TextView) findViewById(R.id.textView1);
+        BlueChat.setTypeface(tf);
+        BlueChat.setText("Blue Chat");
+        BlueChat.setTextSize(TypedValue.COMPLEX_UNIT_SP,50);
+        
+    }
+
+ public void onClick(View v)
+    {
+	 
+    	Intent intent = new Intent(BlueChat_Welcome.this,BlueChat_ProfileBuilder.class);
+    	startActivity(intent);
+    	
+    }
+   
+ @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.blue_chat__welcome, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+       int id = item.getItemId();
+       
+        
+        
+      	if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
